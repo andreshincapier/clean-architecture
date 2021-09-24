194176185b9a67a4e2b4080505f54ff49b0602f5
andreshincapier <andreshincapie82132@gmail.com>
2021-09-24 18:40:20 -0500

    Feature: SonarCloud integration

--- configuration/microservice/src/main/java/com/survey/demo/MainApplication.java
+++ configuration/microservice/src/main/java/com/survey/demo/MainApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-import java.text.NumberFormat;
-import java.util.TimeZone;
-
 @Slf4j
 @SpringBootApplication
 public class MainApplication {
@@ -19,31 +15,8 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
     }
 
-
-    public static void printInfo() {
-        Runtime runtime = Runtime.getRuntime();
-        final NumberFormat format = NumberFormat.getInstance();
-        final long maxMemory = runtime.maxMemory();
-        final long allocatedMemory = runtime.totalMemory();
-        final long freeMemory = runtime.freeMemory();
-        final long mb = 1024L * 1024L;
-        final String mega = " MB";
-
-        log.info("========================== Memory Info ==========================");
-        log.info("Free memory: {}", format.format(freeMemory / mb) + mega);
-        log.info("Allocated memory: {}", format.format(allocatedMemory / mb) + mega);
-        log.info("Max memory: {}", format.format(maxMemory / mb) + mega);
-        log.info("Total free memory: {}", format.format((freeMemory + (maxMemory - allocatedMemory)) / mb) + mega);
-        log.info("=================================================================");
-    }
-
-    @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-    }
-
     @Bean
-    public CommandLineRunner memInfoRunner() {
-        return args -> printInfo();
+    public CommandLineRunner commandLineRunner() {
+        return args -> MemUtils.printInfo();
     }
 }
